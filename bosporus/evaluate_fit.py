77c4ce243bf20816757894bcaa813af4a50da482
annmoel <131354170+annmoel@users.noreply.github.com>
2026-05-04 10:32:13 +0200

    Remove unnecessary blank lines in evaluate_fit.py

--- bosporus/evaluate_fit.py
+++ bosporus/evaluate_fit.py
@@ -7,10 +7,8 @@ def log_likelihood(C_true, C_pred):
     sigma2 = np.mean(residuals**2)
     return -0.5 * n * (np.log(2 * np.pi * sigma2) + 1)
 
-
 def akaike_information_criterion(num_params, log_likelihood_model):
     return 2 * num_params - 2 * log_likelihood_model
-
 
 def relative_likelihood(aic_model, aic_baseline, N):
     return np.exp((aic_baseline - aic_model) / (2 * N))
